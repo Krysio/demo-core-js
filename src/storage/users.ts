b5eb5e7dac37622556f711c1a5f978d5928aa9c2
Krzysio <krzysztof.kozlowski.1992@gmail.com>
2023-12-08 21:48:51 +0100

    Big refactoring: user db

--- src/storage/users.ts
+++ src/storage/users.ts
@@ -1,6 +1,8 @@
 import WBuffer from "@/libs/WBuffer";
 import db, { dbReady } from "./db";
 
+//#region Constants
+
 const EmptyBuffer = WBuffer.from([0]);
 
 export const UserTypeRoot = 0;
@@ -24,12 +26,18 @@ type User = {
     meta: string // JSON, information about account
 };
 
+//#endregion Constants
+//#region Helpers
+
 function enchanceUserRow(row: User): User {
     row.key = WBuffer.from(row.key);
     // TODO areas
 
     return row;
 }
+
+//#endregion Helpers
+//#region Get
 
 export async function getUser(userID: number): Promise<null | User> {
     await dbReady;
@@ -56,41 +64,16 @@ export async function getUser(userID: number): Promise<null | User> {
     return user;
 }
 
-export async function setUser(userData: User): Promise<boolean> {
-    return false;
-}
+//#endregion Get
+//#region Insert
 
-export async function insertRoot(key: WBuffer): Promise<void> {
-    await dbReady;
-
-    const result = await new Promise<void>((resolve, reject) => {
-        db.run(
-            `INSERT INTO users(userID, typeID, level, parentID, key, areas, timeStart, timeEnd, meta) VALUES(0, ${UserTypeRoot}, 0, 0, ?, ?, 0, 0, "")`, 
-            [key, EmptyBuffer],
-            (error: Error & {code: string}) => {
-                if (error) {
-                    if (error.code === 'SQLITE_CONSTRAINT') {
-                        reject(new Error(ErrorDuplicateID));
-                        return;
-                    }
-
-                    reject(new Error(ErrorUnknown));
-                    return;
-                }
-
-                resolve();
-            }
-        );
-    });
-
-    return result;
-}
-
-export async function insertAdmin(adminData: {
+async function insertCommon(adminData: {
     userID: number,
+    typeID: number,
+    level: number,
     parentID: number
     key: WBuffer,
-    level: number,
+    areas: WBuffer,
     timeStart: number,
     timeEnd: number,
     meta: string
@@ -109,23 +92,14 @@ export async function insertAdmin(adminData: {
                 timeStart,
                 timeEnd,
                 meta
-            ) VALUES(
-                ?,
-                ${UserTypeAdmin},
-                ?,
-                ?,
-                ?,
-                ?,
-                ?,
-                ?,
-                ?
-            )`, 
+            ) VALUES( ?, ?, ?, ?, ?, ?, ?, ?, ?)`, 
             [
                 adminData.userID,
+                adminData.typeID,
                 adminData.level,
                 adminData.parentID,
                 adminData.key,
-                EmptyBuffer,
+                adminData.areas,
                 adminData.timeStart,
                 adminData.timeEnd,
                 adminData.meta
@@ -149,3 +123,50 @@ export async function insertAdmin(adminData: {
     return result;
 }
 
+export function insertRoot(key: WBuffer): Promise<void> {
+    return insertCommon({
+        userID: 0,
+        typeID: UserTypeRoot,
+        level: 0,
+        parentID: 0,
+        areas: EmptyBuffer,
+        timeStart: 0,
+        timeEnd: 0,
+        meta: '',
+        key
+    });
+}
+
+export function insertAdmin(adminData: {
+    userID: number,
+    parentID: number
+    key: WBuffer,
+    level: number,
+    timeStart: number,
+    timeEnd: number,
+    meta: string
+}): Promise<void> {
+    return insertCommon({
+        typeID: UserTypeAdmin,
+        areas: EmptyBuffer,
+        ...adminData
+    });
+}
+
+export function insertUser(userData: {
+    userID: number,
+    parentID: number
+    key: WBuffer,
+    level: number,
+    areas: WBuffer,
+    timeStart: number,
+    timeEnd: number,
+    meta: string
+}): Promise<void> {
+    return insertCommon({
+        typeID: UserTypeUser,
+        ...userData
+    });
+}
+
+//#endregion Insert
