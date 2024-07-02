61861c332462dbb70fad4f3a31031fd9964f7a2b
Krzysio <krzysztof.kozlowski.1992@gmail.com>
2024-07-02 09:21:22 +0200

    fix unstable test

--- src/objects/commands/genesis.int.test.ts
+++ src/objects/commands/genesis.int.test.ts
@@ -1,7 +1,7 @@
 import { nodeCreator } from "@/tests/helper";
 
 test('Create a node', async () => {
-    const creator = nodeCreator();
+    const creator = nodeCreator().manualTime(10001);
     const { node, scope: { rootKey } } = creator;
 
     await node.whenInit();
